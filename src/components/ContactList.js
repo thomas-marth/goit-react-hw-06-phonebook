b0d27927283becb1bf72c72697ae35a444180981
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-03-12 00:05:26 +0200

    initial commit

---
+++ src/components/ContactList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ContactListItem from "./ContactListItem";
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul>
+      {contacts.map((contact) => (
+        <li key={contact.id}>
+          <ContactListItem
+            contact={contact}
+            onDeleteContact={onDeleteContact}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactList;
---
+++ src/components/Filter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Filter = ({ value, onChangeFilter }) => (
+  <>
+    <p>Find contacts by name</p>
+    <input name="filter" onChange={onChangeFilter} value={value} />
+  </>
+);
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
+};
+
+export default Filter;
